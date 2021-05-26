82725e6d29d75318eb620806a06ff7396aba0ce5
ryz1g <65825273+ryz1g@users.noreply.github.com>
2021-05-26 16:24:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import kivy
 from datetime import datetime
-from kivy.app import App
 import threading
 import time
 
-from kivy.core.window import Window
-Window.size=(500,800)
-Window.clearcolor = (0/255,0/255,0/255,1)
+from kivy.config import Config
+Config.set('graphics', 'resizable', False)
 
+from kivy.core.window import Window
+from kivy.app import App
+from kivy.graphics import Color,RoundedRectangle
 from kivy.uix.image import Image
 from kivy.uix.label import Label
 from kivy.uix.boxlayout import BoxLayout
@@ -15,28 +16,33 @@ from kivy.lang import Builder
 from kivy.uix.button import Button
 from kivy.uix.screenmanager import ScreenManager,Screen
 
+Window.size=(500,800)
+Window.clearcolor = (0/255,0/255,0/255,1)
+
 text_count=0
 text_dict={}
 dply=0
 
 class text_their(Button):
-    def __init__(self,tx,ssize):
-        super().__init__()
+    def __init__(self,tx,ssize,**kwargs):
+        super(text_their,self).__init__(**kwargs)
         self.text=tx
-        self.background_color=(245/255.0,224/255.0,66/255.0,1)
-        self.font_size=18
+        self.background_normal='Icons/ch2.png'
+        self.background_down='Icons/ch2.png'
+        self.font_size=16
         self.size_hint=(ssize,None)
-        self.size=(ssize,"40dp")
+        self.size=(ssize,"45dp")
         self.pos_hint={'left':1, 'top':1}
 
 class text_your(Button):
-    def __init__(self,tx,ssize):
-        super().__init__()
+    def __init__(self,tx,ssize,**kwargs):
+        super(text_your,self).__init__(**kwargs)
         self.text=tx
-        self.background_color=(235/255.0,125/255.0,103/255.0,1)
-        self.font_size=18
+        self.background_normal='Icons/ch1.png'
+        self.background_down='Icons/ch1.png'
+        self.font_size=16
         self.size_hint=(ssize,None)
-        self.size=(ssize,"40dp")
+        self.size=(ssize,"45dp")
         self.pos_hint={'right':1, 'top':1}
 
 class PigeonApp(App):
@@ -53,9 +59,26 @@ class PigeonApp(App):
     def clock(self,wd):
         global dply
         while True:
-            wd.text=wd.text[:-8]+str(datetime.now())[-15:-7]
+            wd.text=str(datetime.now())[-15:-7]
             time.sleep(1)
-   
+    """
+    def tb_press(self,ind):
+        global text_dict
+        self.ids.t_input=text_dict[ind]
+
+    def show_texts(self):
+        global text_dict
+        print(text_dict)
+
+    def ycam_press(self):
+        c=self.ids.your_cam
+        if c.play:
+            c.play=False
+            #c.texture="images/im1.jpg"
+        else:
+            c.play=True
+            #c.source=""
+    """
     def send(self,t):
         global text_count
         global text_dict
@@ -64,9 +87,9 @@ class PigeonApp(App):
         tx=t.text
         if tx=="":
             return
-        ssize=len(tx)*0.02+0.008
+        ssize=len(tx)*0.02+0.001
         text_dict[str(datetime.now())[:-7]]=tx
-        b=BoxLayout(orientation="vertical", spacing=5,size_hint=(1,None),size=(450,50),pos_hint={'right':1, 'top':1})
+        b=BoxLayout(orientation="vertical", spacing=2,size_hint=(1,None),size=(450,50),pos_hint={'right':1, 'top':1})
         b.add_widget(Label(text="You", font_size=11,size_hint=(None,None),size=("30dp","10dp"),pos_hint={'right':1, 'top':1} ))
         b.add_widget(text_your(tx,ssize))
         dply.add_widget(b)
@@ -81,8 +104,8 @@ class PigeonApp(App):
         if tx=="":
             return
         text_dict[str(datetime.now())[:-7]]=tx
-        ssize=len(tx)*0.02+0.008
-        b=BoxLayout(orientation="vertical", spacing=5,size_hint=(1,None),size=(450,50),pos_hint={'left':1, 'top':1})
+        ssize=len(tx)*0.02+0.001
+        b=BoxLayout(orientation="vertical", spacing=2,size_hint=(1,None),size=(450,50),pos_hint={'left':1, 'top':1})
         b.add_widget(Label(text="Them", font_size=11,size_hint=(None,None),size=("30dp","10dp"),pos_hint={'left':1, 'top':1} ))
         b.add_widget(text_their(tx,ssize))
         dply.add_widget(b)
